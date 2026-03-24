e5c09e3d9f928716a301389adc396d8366ed2ac2
Narayana Yeluru <37355660+Narayana-Yeluru@users.noreply.github.com>
2026-03-23 23:33:08 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,8 @@ resource "azurerm_service_plan" "plan" {
 
 # App Service (Web App)
 resource "azurerm_linux_web_app" "app" {
-  name                = "${var.environment}-webapp-${var.random_suffix}"
+  # name                = "${var.environment}-webapp-${var.random_suffix}"
+  name                =  "${var.environment}-webapp-${random_string.suffix.result}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.plan.id
